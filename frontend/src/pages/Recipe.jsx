07e281b890bb4a96124c43fcdecc01105f0f86bb
Pranjal Maharaniya <pj3511044@gmail.com>
2025-09-19 05:11:50 +0530

    Fix: updated

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -15,7 +15,7 @@ const Recipe = () => {
   const [sortOption, setSortOption] = useState("Default");
 
   const RESULTS_PER_PAGE = 4;
-  const API_URL = process.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     // Animate page load (fade in)
